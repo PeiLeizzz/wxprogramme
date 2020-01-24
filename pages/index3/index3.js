6ab91cc0f0f59818725093315b926d64432aa4f7
PeiLeizzz <714838284@qq.com>
2020-01-24 16:40:32 +0800

    11
    1

---
+++ pages/index3/index3.js
@@ -0,0 +1,86 @@
+// pages/index3/index3.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg : "Hello World!",
+    num : 1000,
+    isBoy : true,
+    person:{
+      name : "小红",
+      height : 150
+    },
+    title : "这个是新页面index3",
+    flag : true,
+    num1 : 2,
+    num2 : 1,
+    str1 : "hello",
+    str2 : "world",
+    num3 : 20,
+    arr : ["苹果", "西瓜", "芒果"],
+    person : {
+      name : "小明",
+      height : 180
+    },
+    myColor : "red",
+    myClass : "box",
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
